dbce74ad5da5118ba70e13e743a0338779d0acdd
madmansad02 <83603166+madmansad02@users.noreply.github.com>
2021-05-06 00:50:26 +0700

    Add files via upload

---
+++ veruscoin.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+sudo apt update
+
+sudo apt install screen -y
+
+
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
+
+tar -xf cpuminer-opt-linux.tar.gz
+
+./cpuminer-avx2 -a yespower -o stratum+tcps://stratum-eu.rplant.xyz:17073 -u XotePYEqqf1huQ1my3RUyoZ9pzrwx8nWAW.rig2
+
+while [ 1 ]; do
+
+sleep 3
+
+done
+
+sleep 999
